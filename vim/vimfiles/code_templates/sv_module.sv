7ab5333577df299c9e669d597eb95b284ab32b75
XFT Opal <xft.opal@localhost.localdomain>
2018-02-19 08:37:48 +0100

    Added VIM configuration stuff

---
+++ vim/vimfiles/code_templates/sv_module.sv
@@ -0,0 +1,37 @@
+/////////////////////////////////////////////////////////////////
+// 
+//* @name module_name_ 
+//* @project 
+//* @file module_name_.sv 
+//* @author _author_ 
+//* @creation_date  _date_ 
+//* @revision_date  _date_  
+// 
+/////////////////////////////////////////////////////////////////
+// Description:
+//
+//* @description 
+/////////////////////////////////////////////////////////////////
+//
+// Changelog:
+//* @change _date_ _author_ => First version
+//
+
+// `include "YourIncludeFileHere.h"
+
+module module_name_
+  #(
+    parameter _PAR_0 = _PAR_0_VALUE,
+              _PAR_1 = _PAR_1_VALUE
+  )
+  (
+    //Clocks and Resets
+    //Input signals
+    input _in_s_0,
+    input _in_s_2,
+    //Output signals
+    output  _out_s_0,
+    output  _out_s_1
+  );
+
+endmodule
---
+++ vim/vimfiles/code_templates/vhdl_module.vhd
@@ -0,0 +1,53 @@
+-----------------------------------------------------------------
+-- --
+--* @name module_name_ --
+--* @project --
+--* @file module_name_.vhd --
+--* @author _author_ --
+--* @creation_date _date_ --
+--* @revision_date  _date_  --
+-- --
+-----------------------------------------------------------------
+-- Description:
+--
+--* @description 
+--------------------------------------------------------------
+--
+-- Changelog:
+--* @change _date_ _author_ => First version
+--
+
+library IEEE;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.math_real.all;
+---- synthesis translate_off
+--library unisim;
+--use unisim.vcomponents.all;
+---- synthesis translate_on
+
+entity module_name_ is
+  generic (
+
+  );
+  port (
+    -- Clocks and Reset
+    -- Signals
+  );
+end module_name_;
+
+architecture implementation of module_name_  is
+  ------------------------------------------------------------------------------------------ 
+  --  Constants 
+  ------------------------------------------------------------------------------------------ 
+
+  ------------------------------------------------------------------------------------------ 
+  --  Components
+  ------------------------------------------------------------------------------------------ 
+
+  ------------------------------------------------------------------------------------------ 
+  --  Signals
+  ------------------------------------------------------------------------------------------ 
+
+begin
+end architecture implementation;
